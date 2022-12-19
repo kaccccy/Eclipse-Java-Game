205f3534553cd170a32aa3bfaaa838378471d3f1
kaccccy <113552939+kaccccy@users.noreply.github.com>
2022-12-18 23:48:30 -0700

    changed game title

---
+++ CapyGame2/main/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		MappedBackgroundAnimation animation = new MappedBackgroundAnimation();
+		AnimationFrame frame = new AnimationFrame((Animation)animation);
+		frame.start();
+	}
+
+}
